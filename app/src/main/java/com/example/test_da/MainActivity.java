e6f6c2a79fc8651ccfa9e5c0aa88d9838e481e06
dieptvph40380ị <dieptvph40380@fpt.edu.vn>
2023-11-15 17:03:09 +0700

    tesst

--- app/src/main/java/com/example/test_da/MainActivity.java
+++ app/src/main/java/com/example/test_da/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //jgf
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///tesst
     }
 }
